c58fbda28540b0cbd242f5d8f5acb8b55104910c
EC2 Default User <ec2-user@ip-172-31-39-123.ap-northeast-1.compute.internal>
2024-05-18 01:49:55 +0000

    played py commit

---
+++ 01_python/basic_print3.py
@@ -0,0 +1,2 @@
+#/usu/bin/python
+print("Hello","world", "This is Python", sep="!")
---
+++ 01_python/basic_print4.py
@@ -0,0 +1,7 @@
+#/usr/bin/python
+print("first", end="\n"); print("second")
+
+print("third", end=""); print("fourth")
+
+print("fifth", end=","); print("sixth")
+

3f551af4a2e5f95c041b97e0698b6b3545c65ba4
melimj <melissa.mj.0117@gmail.com>
2024-06-08 11:18:50 +0900

    20240608 pracice cmd_until

--- 00_bash/cmd_until.sh
+++ 00_bash/cmd_until.sh
@@ -1,4 +1,4 @@
-#while文を使用し、入力された値をデクリメントしながら表示し、
+#until文を使用し、入力された値をデクリメントしながら表示し、
 #5になった時点で終了するスクリプトを作成せよ。
 #
 #5未満の入力はエラーメッセージを表示すること。
@@ -16,18 +16,15 @@ input=0
 read -p "Press the any number, but higher than 5: " input
 
 if [ $input -gt 5 ]; then 
-#if [ $input -ge 5 ]; then 
 	echo "Start degree the number until 5." 
-
-	while [ $input -gt 5 ]; do
-	#while [ $input -ge 5 ]; do
-		input=`expr $input - 1`
+	
+	until [ $input -eq 5 ]; do
+	input=`expr $input - 1`
 		echo "$input"
 		sleep 1
-	done
+done
 	echo "Finished"
 else
-#else [ $input -lt 5 ]; then
 	echo "Do not need to degree the number. It's same or lower than 5."
 fi
 

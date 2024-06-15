9e883f23321eb957492672c30ca4886ae8f82c26
melimj <melissa.mj.0117@gmail.com>
2024-06-15 10:24:45 +0900

    20240615 brush up cmd_expr.sh

--- 00_bash/cmd_expr.sh
+++ 00_bash/cmd_expr.sh
@@ -7,22 +7,24 @@
 a=10
 b=5
 c=`expr $a - $b`
-d=`expr $a / $b`
+d=`expr $a '/' $b`
 e=`expr $a % $b`
-f=`expr $a*$b`
-g=`expr $a * $b`
-h=`expr $a // $b`
-
+f=`expr $a '*' $b`
 i=`echo "($a*$b)"`
+g=`expr $a * $b`
+h=`expr $a / $b`
+j=`expr $a + $b`
 
 #print
-echo $c 
-echo $d
-echo $e
-echo $f
+echo "10 + 5=$j"
+echo "10 - 5=$c"
 
-echo $i
+echo "10 % 5=$e"
 
+echo "10 / 5=$h"
+echo "10 '/' 5=$d"
+
+echo "10 '*' 5=$f"
+echo "(10*5)=$i"
 #print_err
-echo $g
-echo $h
+echo "10 * 5=$g   #error"
